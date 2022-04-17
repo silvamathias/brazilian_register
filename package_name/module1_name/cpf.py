b3c59740c34044dc23b63e3c854924440e3370f5
phelipe <silvamathias@gmail.com>
2022-04-17 00:13:15 -0300

    cod cpf funcionando

--- package_name/module1_name/cpf.py
+++ package_name/module1_name/cpf.py
@@ -1,36 +1,71 @@
-input_number = input('Informe o número do doc:')
-def clear_number(text_number):
-    num = ''
-    for n in text_number:
-        if n in ('0','1','2','3','4','5','6','7','8','9'):
-            num += n
+#input_number = input('Informe o número do doc:')
+class cpf:
+    def __init__(self, cpf):
+        self.cpf_input = cpf
+        self.cpf = self._clear_cpf()
+        self.cpf_itens = self.cpf[:9]
+        self.cpf_cod_in = self.cpf[9:11]
+        self.cpf_output = self._calc_cpf_out()
+        self.cpf_cod_out = self.cpf_output[9:11]
 
-    return num
+    def _clear_cpf(self):
+        num = ''
+        for n in self.cpf_input:
+            if n in ('0','1','2','3','4','5','6','7','8','9'):
+                num += n
 
-def cpf_test(num_value):
-    nr = num_value%11
-    if nr <=1:
-        nt = '0'
-    else:
-        nt = 11 - nr
+        return num
 
-    return int(nt)
+    def _cod_test(self,num_value):
+        nr = num_value%11
+        if nr <=1:
+            nt = '0'
+        else:
+            nt = 11 - nr
 
-doc = clear_number(input_number)
-#cpf
-if len(doc) == 11:
-    doc_input = doc[:9]
-    inv_doc = doc_input[::-1]
-    while len(inv_doc) < 11:
-        vl_sum = 0.0
-        k = 2
-        for i in inv_doc:
-            vl_sum += (int(i) * k)
-            k+=1
+        return int(nt)
 
-        c1 = cpf_test(vl_sum)
-        inv_doc = str(c1) + str(inv_doc)
+    def _calc_cpf_out(self):
+        #cpf
+        inv_cpf = self.cpf_itens[::-1]
+        while len(inv_cpf) < 11:
+            itens_sum = 0.0
+            k = 2
+            for i in inv_cpf:
+                itens_sum += (int(i) * k)
+                k+=1
 
-new_doc = inv_doc[::-1]
-print(doc)
-print(new_doc)
+            c1 = self._cod_test(itens_sum)
+            inv_cpf = str(c1) + str(inv_cpf)
+
+        return inv_cpf[::-1]
+
+    def cpf_entrada(self):
+
+        return self.cpf
+
+    def cpf_retorno(self):
+
+        return self.cpf_output
+
+    def cpf_verificador_entrada(self):
+
+        return self.cpf_cod_in
+
+    def cpf_verificador_retorno(self):
+
+        return self.cpf_cod_out
+
+    def cpf_status(self):
+        if self.cpf == self.cpf_output:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    tc = cpf('111.222.333-96')
+    print(tc.cpf_status())
+    print(tc.cpf_entrada())
+    print(tc.cpf_retorno())
+    print(tc.cpf_verificador_entrada())
+    print(tc.cpf_verificador_retorno())
