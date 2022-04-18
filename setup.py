69e08eab30cc61dbf67d4f3e9b387a0200d6be0d
phelipe <silvamathias@gmail.com>
2022-04-17 21:54:49 -0300

    Commit depois de publicar no test pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Tools to test brazilian register numbers (CPF or CNPJ)",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/silvamathias/dio_desafio_ciacao_pacote_python"
+    url="https://github.com/silvamathias/dio_desafio_ciacao_pacote_python",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.7',
